168d7f93fcc23276df80db51a71c944ecb444bb1
Joshua Cahill <tupperwareC@hotmail.com>
2019-06-16 22:15:42 +1000

    Added instructions

---
+++ instructions.sh
@@ -0,0 +1,13 @@
+# Install Java
+sudo yum -y install java-1.8.0
+
+# Create a directory
+mkdir minecraftserver
+cd minecraftserver
+
+# Download the latest version of minecraft
+wget https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar
+
+# Run minecraft in server mode
+sudo java -Xmx1G -Xms1G -jar server.jar nogui
+
