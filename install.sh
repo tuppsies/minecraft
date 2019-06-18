58ff2ca48131f7c9750e8b5ae791c14a1ebba806
tuppsies <tupperwarec@hotmail.com>
2019-06-18 03:10:44 +0000

    Removed empty lines

--- install.sh
+++ install.sh
@@ -7,4 +7,3 @@ cd minecraftserver
 
 # Download the latest version of minecraft
 wget https://launcher.mojang.com/v1/objects/808be3869e2ca6b62378f9f4b33c946621620019/server.jar
-
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 # Run minecraft in server mode
 sudo java -Xmx1G -Xms1G -jar server.jar nogui
-
